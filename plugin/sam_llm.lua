339e62a30b39c55ae44b1678988ffdc1f6675d7e
Samuel Brown <sam@samuelbrown.me>
2025-06-08 16:59:09 +1000

    Setup wasn't run sadly

--- plugin/sam_llm.lua
+++ plugin/sam_llm.lua
@@ -1,5 +1,7 @@
 local sam_llm = require("sam_llm")
 
+sam_llm.setup(nil)
+
 vim.api.nvim_create_user_command(
   "LlmProcess",                      -- :LlmProcess / :llmprocess / :llm_process
   function(opts)                     -- opts.args   -> entire arg string
